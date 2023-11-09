c01ee8a0ba52980b0525a2ba483989c12bbec022
FatoumataCissoko <144293052+FatoumataCissoko@users.noreply.github.com>
2023-11-09 13:58:54 -0500

    init

---
+++ gestion/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MON TP 2</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Combien d'adresse voulez_vous saisir: </h1>
+        <form action="formulaires.php" method="post">
+            <label for="nombre_Adresse">Nombre d'adresse: </label>
+            <input type="number" name="nombre_Adresse" required>
+            <input type="submit" values="Submit">
+        </form>
+
+    </div>
+</body>
+</html>
