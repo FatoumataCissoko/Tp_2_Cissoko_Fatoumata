adb77905866f9a3b2879ce87b1d88c96687ba945
FatoumataCissoko <144293052+FatoumataCissoko@users.noreply.github.com>
2023-11-12 09:54:52 -0500

    init

--- gestion/formulaires.php
+++ gestion/formulaires.php
@@ -1,11 +1,11 @@
 <a href="index.php">Retour a l'accueil</a>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Récupérer le nombre d'adresse et s'assurer que sa existe dans les données POST:
+    // Recuperer le nombre d'adresse et s'assurer que sa existe dans les donnees POST:
 
     $nombre_Adresse = isset($_POST["nombre_Adresse"]) ? intval($_POST["nombre_Adresse"]) : 0;
    
-    // Vérifier que le nombre est positif:
+    // Verifier que le nombre est positif:
         if ($nombre_Adresse <= 0) {
             echo "<p>Le nombre d'adresses doit être positif SVP.</p>";
         } else {
@@ -14,28 +14,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "<h2>Remplissez les adresses</h2>";
         echo "<form action='result.php' method='post'>";
         
-        //La saisie des adresses par l'utisteur:
-                //STREET
+        //La saisie des adresses par l'utilisateur:
+                //STREET:
         for ($i = 1; $i <= $nombre_Adresse; $i++) {
             echo "<div class='address-form'>";
             echo "<h2> Adresse $i</h2>";
             echo "<label for='street_$i'>Street:</label>";
             echo "<input type='text' name='street_$i' maxlength='60' required>";
-                    //STREET_NB
+                    //STREET_NB:
             echo "<label for='street_nb_$i'>Street_nb:</label>";
             echo "<input type='number' name='street_nb_$i' required>";
-                    //TYPE
+                    //TYPE:
             echo "<label for='type_$i'>Type:</label>";
             echo "<input type='text' name='type_$i' maxlength='30' required>";
-                    //HINT
-            echo "<label for='hint_$i'>Hint:</label>";
-            echo "<select name='hint_$i' required>";
-                    //LIVRAISON,FACTURATION et LIEU
-            echo "<option value='livraison'>Livraison</option>";
-            echo "<option value='facturation'>Facturation</option>";
-            echo "<option value='lieu'>Lieu</option>";
-            echo "</select>";
-                //CITY
+                //CITY:
             echo "<label for='city_$i'>City:</label>";
             echo "<select name='city_$i' required>";
             echo "<option value='Montreal'>Montreal</option>";
@@ -43,16 +35,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             echo "<option value='Toronto'>Toronto</option>";
             echo "<option value='Quebec'>Quebec</option>";
             echo "</select>";
-                    //ZIPCODE
+                    //ZIPCODE:
             echo "<label for='zipcode_$i'>Zip Code:</label>";
-            echo "<input type='text' name='zipcode_$i' pattern='{6}\w' title='Six carateres required' required>";
+            echo "<input type='text' name='zipcode_$i' pattern='\w{6}' title='Six carateres required' required>";
 
             echo "<br>";
             echo "</div>";
 
 
         }
-    echo "<input type='hidden' name='nombre_addresse' value='$nombre_Adresse'>";
+    echo "<input type='hidden' name='nombre_Adresse' value='$nombre_Adresse'>";
     echo "<br>";
     echo "<input type='submit' value='Soumettre'>";
     
@@ -65,23 +57,67 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 }
      
 }
-?>
 
-<?php
+// Connexion à la BD
+$server_name = 'localhost';
+$user_name = "root";
+$pwd = "";
+$BaseDonnees_name = "ecom1_tp2";
+
+$connexion = mysqli_connect($server_name, $user_name, $pwd, $BaseDonnees_name);
+
+// Validation de la connexion
+if ($connexion->connect_error) {
+    die("Erreur de connexion " . $connexion->connect_error);
+}
+
+$connexion->select_db($BaseDonnees_name);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $stmt = $connexion->prepare("INSERT INTO address (street, street_nb, type, city, zipcode) VALUES (?, ?, ?, ?, ?)");
+
+    if (!$stmt) {
+        die("Erreur de préparation de la requête: " . $connexion->error);
+    }
+
+    $stmt->bind_param("sisss", $street, $street_nb, $type, $city, $zipcode);
+
+    for ($i = 1; $i <= $nombre_Adresse; $i++) {
+        $street = $_POST["street_$i"];
+        $street_nb = $_POST["street_nb_$i"];
+        $type = $_POST["type_$i"];
+        $city = $_POST["city_$i"];
+        $zipcode = $_POST["zipcode_$i"];
+        
+
+       
+if (!$stmt->execute()) {
+            die("Erreur d'exécution de la requête: " . $stmt->error);
+        }
+    }
+
+    $stmt->close();
+}
+
+$connexion->close();
+
+
+
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Affichage des adresses soumises pour la validation
+    // Affichage des adresses soumises pour la validation:
     echo "<div class='container'>";
     echo "<h2>Adresse soumise</h2>";
     
-        for ($i = 1; $i <= $_POST["nombre_Adresse"]; $i++) {
+        for ($i = 1; $i <= $nombre_Adresse; $i++) {
             echo "<div class='adresse-result'>";
             echo "<h2>Adresse $i</h2>";
             echo "<p><strong> Street: </strong> " . htmlspecialchars($_POST["street_$i"]) . "</p>";
             echo "<p><strong> Street_nb: </strong> " . htmlspecialchars($_POST["street_nb_$i"]) . "</p>";
             echo "<p><strong> Type: </strong> " . htmlspecialchars($_POST["type_$i"]) . "</p>";
-            echo "<p><strong> Hint: </strong> " . htmlspecialchars($_POST["hint_$i"]) . "</p>";
             echo "<p><strong> City: </strong> " . htmlspecialchars($_POST["city_$i"]) . "</p>";
             echo "<p><strong> Zip Code: </strong> " . htmlspecialchars($_POST["zipcode_$i"]) . "</p>";
             echo "<br>";
--- resultat/result.php
+++ resultat/result.php
@@ -1,5 +1,55 @@
-<a href="formulaires.php">Page dinsertion</a>
+<a href="formulaires.php">Page dinsertion</a><br>
 <a href="index.php">Accueil</a>
+
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Connexion a la BD :
+    $server_name = 'localhost';
+    $user_name = "root";
+    $pwd="";
+    $BaseDonnees_name = "ecom1_tp2";
+    
+    $connexion =  mysqli_connect($server_name,$user_name,$pwd,$BaseDonnees_name);
+
+    //Validation de la connexion :
+
+    if ($connexion->connect_error){
+
+        echo("Erreur de connexion " .$connexion->connect_error);
+    }
+
+    //Preparation de ma requete SQL d'insertion :
+
+    $stmt = $connexion->prepare("INSERT INTO address (street, street_nb, type, city, zipcode) 
+    VALUES (?, ?, ?, ?, ?)");
+
+    // Liaison entre les paramètres :
+
+    $stmt->bind_param("sisss", $street, $street_nb, $type, $city, $zipcode);
+
+}
+
+// Insertion de chaque champs d’adresses :
+for ($i = 1; $i <= $_POST["nombre_Adresse"]; $i++) {
+        
+    $street = $_POST["street_$i"];
+    $street_nb = $_POST["street_nb_$i"];
+    $type = $_POST["type_$i"];
+    $city = $_POST["city_$i"];
+    $zipcode = $_POST["zipcode_$i"];
+    if (!$stmt->execute()) {
+        die("Erreur d'exécution de la requête: " . $stmt->error);
+    //$stmt->execute();
+    }
+
+}
+
+
+// Fermeture de la requête
+$stmt->close();
+
+// Pour close mes connexions
+$connexion->close();
 
 ?>
